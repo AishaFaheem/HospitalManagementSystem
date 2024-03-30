67d000ca920064c85c5f130a2eedcd5b1cc87b05
Aisha Faheem <64909342+AishaFaheem@users.noreply.github.com>
2024-03-31 04:09:42 +0500

    Delete core directory

--- core/forms.py
+++
@@ -1,8 +0,0 @@
-# core/forms.py
-from django import forms
-from .models import Patient
-
-class PatientForm(forms.ModelForm):
-    class Meta:
-        model = Patient
-        fields = ['name', 'insurance_id', 'gender', 'is_insurance_verified']
